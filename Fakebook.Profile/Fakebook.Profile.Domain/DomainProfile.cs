9000bd5b83ea843b45f90410178fb57759dd29ab
dppet <dppet@DANIELOMEN>
2020-12-29 13:20:45 -0800

    Creaited Domain Model

---
+++ Fakebook.Profile/Fakebook.Profile.Domain/DomainProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fakebook.Profile.Domain
+{
+    class DomainProfile
+    {
+
+        public string Email { get; set; }
+        public string ProfilePictureUrl { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return FirstName+' '+LastName;
+            }
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Status { get; set; }
+    }
+}
