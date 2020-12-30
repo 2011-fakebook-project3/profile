b9928127d919cec6699ee46cfb739c393f1774d6
dppet <dppet@DANIELOMEN>
2020-12-30 08:18:19 -0800

    email tests

--- Fakebook.Profile/Fakebook.Profile.UnitTests/DomainTests/ProfileModelTests.cs
+++ Fakebook.Profile/Fakebook.Profile.UnitTests/DomainTests/ProfileModelTests.cs
@@ -12,16 +12,68 @@ namespace Fakebook.Profile.UnitTests.DomainTests
 {
     public class ProfileModelTests
     {
+        #region NameTests
         [Theory]
         [InlineData("first", "last")]
+        [InlineData("Something", "Name")]
         public void NameReturnsProperFormat(string first, string last)
         {
             //arrange
-            DomainProfile profile = new DomainProfile();
+            DomainProfile profile = new DomainProfile("example@email.com");
+
+            profile.FirstName = first;
+            profile.LastName = last;
 
             //act 
+            string fullname = profile.Name;
 
             //assert
+            Assert.NotNull(fullname);
+            Assert.Equal(first + ' ' + last, fullname);
         }
+        #endregion
+
+
+        #region EmailTests
+        [Fact]
+        public void EmailCantBeNull()
+        {
+            //arrange
+            string email = null;
+            //act
+            //assert
+            Assert.Throws<ArgumentNullException>(() => new DomainProfile(email));
+        }
+
+
+        [Theory]
+        [InlineData("test1@test")]
+        [InlineData("test2.test2")]
+        [InlineData("test3.test3@test3")]
+        public void InvalidEmailThorwsException(string email)
+        {
+            //arrange
+            //act
+            //assert
+            Assert.Throws<ArgumentException>(() => new DomainProfile(email));
+        }
+
+
+        [Theory]
+        [InlineData("Simple@email.com")]
+        [InlineData("Other@email.test")]
+        [InlineData("subdomianEmai@puppies.test.mail")]
+        [InlineData("notcom@puppies.supplies")]
+        public void ValidEmailWorks(string email)
+        {
+            //arrange
+            //act
+            DomainProfile profile = new DomainProfile(email);
+
+            //assert
+            Assert.NotNull(profile.Email);
+            Assert.Equal(email, profile.Email);
+        }
+        #endregion
     }
 }
