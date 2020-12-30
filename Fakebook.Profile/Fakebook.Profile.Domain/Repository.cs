325ea5b29bc6151dce4779e027801b08f28d2e0d
dppet <dppet@DANIELOMEN>
2020-12-30 10:29:24 -0800

    add methods to repositroy

--- Fakebook.Profile/Fakebook.Profile.Domain/Repository.cs
+++ Fakebook.Profile/Fakebook.Profile.Domain/Repository.cs
@@ -8,7 +8,17 @@ namespace Fakebook.Profile.Domain
 {
     class Repository : IRepository
     {
+        public Task CreateProfileAsync(DomainProfile profileData)
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task<DomainProfile> GetProfileAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProfileAsync(string email, DomainProfile domainProfileData)
         {
             throw new NotImplementedException();
         }
