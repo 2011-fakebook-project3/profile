6c9d3f03dc64d25d7cdf7f9ea30f12d55c23ac04
dppet <dppet@DANIELOMEN>
2020-12-30 10:29:08 -0800

    inital code skeleton from example

--- Fakebook.Profile/Fakebook.Profile.DataAccess/BlobService.cs
+++ Fakebook.Profile/Fakebook.Profile.DataAccess/BlobService.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace Fakebook.Profile.DataAccess
 {
@@ -11,11 +14,32 @@ namespace Fakebook.Profile.DataAccess
     /// </summary>
     class BlobService : IBlobService
     {
-        //todo: replace with the real type
-        private string _Client = null;
+        private readonly BlobServiceClient _Client;
+        private string _ContainerName;
 
-        public BlobService(string client){
+        public BlobService(BlobServiceClient client, string containerName){
             _Client = client;
+        }
+
+        //test
+        public async Task<Uri> UploadToBlob(Stream content, string contentType, string fileName, string blobContainerName=null)
+        {
+            if(blobContainerName is not null)
+            {
+                blobContainerName = this._ContainerName;
+            }
+
+            BlobContainerClient blobclient = GetClient(blobContainerName);
+
+            await blobclient.GetBlobClient(fileName).UploadAsync(content, new BlobHttpHeaders { ContentType = contentType});
+
+            return blobclient.Uri;
+        }
+
+        private BlobContainerClient GetClient(string containerName)
+        {
+            BlobContainerClient containerClient = _Client.GetBlobContainerClient(containerName);
+            return containerClient;
         }
     }
 }
