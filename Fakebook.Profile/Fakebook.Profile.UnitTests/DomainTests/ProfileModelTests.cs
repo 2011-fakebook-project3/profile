b695dfa159251d65cb9c73eecb7cd7481a882870
dppet <dppet@DANIELOMEN>
2020-12-30 09:14:35 -0800

    More Test Work

--- Fakebook.Profile/Fakebook.Profile.UnitTests/DomainTests/ProfileModelTests.cs
+++ Fakebook.Profile/Fakebook.Profile.UnitTests/DomainTests/ProfileModelTests.cs
@@ -12,28 +12,6 @@ namespace Fakebook.Profile.UnitTests.DomainTests
 {
     public class ProfileModelTests
     {
-        #region NameTests
-        [Theory]
-        [InlineData("first", "last")]
-        [InlineData("Something", "Name")]
-        public void NameReturnsProperFormat(string first, string last)
-        {
-            //arrange
-            DomainProfile profile = new DomainProfile("example@email.com");
-
-            profile.FirstName = first;
-            profile.LastName = last;
-
-            //act 
-            string fullname = profile.Name;
-
-            //assert
-            Assert.NotNull(fullname);
-            Assert.Equal(first + ' ' + last, fullname);
-        }
-        #endregion
-
-
         #region EmailTests
         [Fact]
         public void EmailCantBeNull()
@@ -50,7 +28,7 @@ namespace Fakebook.Profile.UnitTests.DomainTests
         [InlineData("test1@test")]
         [InlineData("test2.test2")]
         [InlineData("test3.test3@test3")]
-        public void InvalidEmailThorwsException(string email)
+        public void InvalidEmailThrowsException(string email)
         {
             //arrange
             //act
@@ -75,5 +53,176 @@ namespace Fakebook.Profile.UnitTests.DomainTests
             Assert.Equal(email, profile.Email);
         }
         #endregion
+
+
+        #region PfPTests
+        [Fact]
+        public void UrlCanBeNull()
+        {
+            //arrange
+            Uri url = null;
+            DomainProfile profile = new DomainProfile("test@test.test");
+
+            //act
+            profile.ProfilePictureUrl = url;
+
+            //assert
+            Assert.Null(profile.ProfilePictureUrl);
+        }
+
+        [Theory]
+        //https://imgur.com/t/photography/nOfAU66
+        [InlineData("imgur.com","t/photography/nOfAU66")]
+        public void ValidUrlWorks(string host, string path)
+        {
+            //arrange
+            DomainProfile profile = new DomainProfile("test@test.com");
+            UriBuilder uriBuilder = new UriBuilder();
+            uriBuilder.Host = host;
+            uriBuilder.Path = path;
+
+            //act
+            Uri uri = uriBuilder.Uri;
+
+            //assert
+            Assert.NotNull(profile.ProfilePictureUrl);
+        }
+
+        #endregion
+
+
+        #region NameTests
+        [Theory]
+        [InlineData("first", "last")]
+        [InlineData("Something", "Name")]
+        public void NameReturnsProperFormat(string first, string last)
+        {
+            //arrange
+            DomainProfile profile = new DomainProfile("example@email.com");
+
+            profile.FirstName = first;
+            profile.LastName = last;
+
+            //act 
+            string fullname = profile.Name;
+
+            //assert
+            Assert.NotNull(fullname);
+            Assert.Equal(first + ' ' + last, fullname);
+        }
+
+
+        //name test - only valid characters
+        [Theory]
+        [InlineData("Name")]
+        [InlineData("name")]
+        [InlineData("hypen-name")]
+        [InlineData("Na,me")]
+        [InlineData("Mr.Name")] // <- forgot periods
+        public void ValidNameAccepted(string name)
+        {
+            //arrange
+            DomainProfile profile = new DomainProfile("email@example.com");
+
+            //act
+            profile.FirstName = name;
+            profile.LastName = name;
+
+            //assert
+            Assert.Equal(name, profile.FirstName);
+            Assert.Equal(name, profile.LastName);
+        }
+
+        [Theory]
+        [InlineData("X Æ A-12")]
+        [InlineData("1234567")]
+        [InlineData("l33t")]
+        [InlineData("!@#$%^&*()_+")]
+        public void InvalidNameRejected(string name)
+        {
+            //arrange
+            DomainProfile profile = new DomainProfile("email@example.com");
+
+            //act
+            //assert
+            Assert.Throws<ArgumentException>(() => profile.FirstName = name);
+            Assert.Throws<ArgumentException>(() => profile.LastName = name);
+        }
+        #endregion
+
+        #region PhoneNumberTests
+
+        [Theory]
+        [InlineData("1234567899")]
+        [InlineData("570-386-1123")]
+        [InlineData("(267)-424-2243")]
+        [InlineData("(267) 424-2243")]
+        [InlineData("267 424 2243")]
+        public void ValidPhoneNumberWorks(string phoneNumber)
+        {
+            // arrange
+            DomainProfile profile = new DomainProfile("testemail@email.com");
+
+            // act
+            profile.PhoneNumber =  phoneNumber;
+           
+            // assert
+            Assert.True(profile.PhoneNumber.Length <= 14 || profile.PhoneNumber.Length >= 10);
+        }
+
+        [Theory]
+        [InlineData("345")]
+        [InlineData("77777777777777777777")]
+        [InlineData("dw#oi3456")]
+        public void InvalidPhoneNumberThrowsException(string phoneNumber)
+        {
+            // arrange
+            DomainProfile profile = new DomainProfile("testemail@email.com");
+
+            // act
+            // assert
+            Assert.Throws<ArgumentException>(() => profile.PhoneNumber =  phoneNumber);
+        }
+
+
+        #endregion
+
+        #region BirthdayTests
+        [Theory]
+        [InlineData("1888-08-08")]
+        [InlineData("1999-09-09")]
+        [InlineData("2020-12-29")]
+        public void ValidBirthDataShouldBeInThePast(DateTime BirthDate)
+        {
+            // arrange
+            DomainProfile profile = new DomainProfile("testemail@email.com");
+
+            // act
+            profile.BirthDate = BirthDate;
+
+            // assert
+            Assert.True(profile.BirthDate < DateTime.Now);
+
+        }
+
+        [Theory]
+        [InlineData("2021-05-11")]
+        [InlineData("2022-02-28")]
+        [InlineData("2023-12-31")]
+        public void InvalidBirthdateShouldThrowAnException(DateTime BirthDate)
+        {
+            // arrange 
+            DomainProfile profile = new DomainProfile("testemail@email.com");
+
+            // act
+            profile.BirthDate = BirthDate;
+
+            // assert
+            Assert.Throws<ArgumentException>(() => profile.BirthDate = BirthDate);
+
+        }
+        #endregion
+
+
     }
 }
