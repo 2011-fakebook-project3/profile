b6a87eedcb84a913b1e92ca6a5596613e8540dbc
dppet <dppet@DANIELOMEN>
2020-12-30 15:06:42 -0800

    imports

--- Fakebook.Profile/Fakebook.Profile.UnitTests/DomainTests/ProfileTests.cs
+++ Fakebook.Profile/Fakebook.Profile.UnitTests/DomainTests/ProfileTests.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Fakebook.Profile.Domain;
+using Fakebook.Profile.UnitTests.TestData;
 using Moq;
 
 using Xunit;
