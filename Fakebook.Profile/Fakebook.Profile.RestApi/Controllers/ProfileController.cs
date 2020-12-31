602b14522e4a3bf2af861ce3d2c7dfc626b90fcd
Damien Bevins <damien.bevins@outlook.com>
2020-12-31 10:30:16 -0600

    Change ProfileController from MvcController to ApiController.

--- Fakebook.Profile/Fakebook.Profile.RestApi/Controllers/ProfileController.cs
+++ Fakebook.Profile/Fakebook.Profile.RestApi/Controllers/ProfileController.cs
@@ -1,87 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fakebook.Profile.RestApi.Controllers
 {
-    public class ProfileController : Controller
+    [Route("api/user")]
+    [ApiController]
+    public class ProfileController : ControllerBase
     {
-        // GET: Profile
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: Profile/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
         // GET: Profile/Create
-        public ActionResult Create()
-        {
-            return View();
+        public ActionResult Create() {
+            // return default ok for now
+            return Ok();
         }
 
         // POST: Profile/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Profile/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
+        public ActionResult Create(IFormCollection collection) {
+            // return default ok for now
+            return Ok();
         }
 
         // POST: Profile/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Profile/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Profile/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+        public ActionResult Edit(int id, IFormCollection collection) {
+            // return default ok for now
+            return Ok();
         }
     }
 }
