ec037a75c85c3ccffa1d6f1d1bbaae495fc326ed
dppet <dppet@DANIELOMEN>
2020-12-31 09:27:16 -0800

    Email Tests for the Blob Model

---
+++ Fakebook.Profile/Fakebook.Profile.UnitTests/APITests/BlobModelTests/BlobProfileModelTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+using Fakebook.Profile.DataAccess.BlobModel;
+
+namespace Fakebook.Profile.UnitTests.APITests.BlobModelTests
+{
+    public class BlobProfileModelTests
+    {
+
+        [Theory]
+        [InlineData("test@email.com")]
+        [InlineData("random@email.com")]
+        [InlineData("Simple@email.com")]
+        [InlineData("Other@email.test")]
+        [InlineData("subdomianEmai@puppies.test.mail")]
+        [InlineData("notcom@puppies.supplies")]
+        public void SetEmailShouldWork(string email)
+        {
+            //arrange
+            BlobProfile bf = new BlobProfile();
+
+            //act
+            bf.Email = email;
+
+            //assert
+            Assert.NotNull(bf.Email);
+            Assert.Equal(email, bf.Email);
+        }
+
+
+        //note: maybe this shouldn't work, but
+        // it's the model's responsibility for validation 
+        [Theory]
+        [InlineData("test@email.com")]
+        [InlineData("random@email.com")]
+        [InlineData("Simple@email.com")]
+        [InlineData("Other@email.test")]
+        [InlineData("subdomianEmai@puppies.test.mail")]
+        [InlineData("notcom@puppies.supplies")]
+        public void ChangingEmailShouldWork(string email)
+        {
+            //arrange
+            BlobProfile bf = new BlobProfile();
+            bf.Email = "someOtherEmail@email.com";
+
+            //act
+            bf.Email = email;
+
+            //assert
+            Assert.NotNull(bf.Email);
+            Assert.Equal(email, bf.Email);
+        }
+
+    }
+}
