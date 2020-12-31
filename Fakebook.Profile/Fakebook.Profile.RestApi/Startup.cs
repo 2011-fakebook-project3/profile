e9a2607f50a8b06445f88bb75ef97e8f86536b6b
dppet <dppet@DANIELOMEN>
2020-12-31 10:20:15 -0800

    Added empty controller so this proj compiles

--- Fakebook.Profile/Fakebook.Profile.RestApi/Startup.cs
+++ Fakebook.Profile/Fakebook.Profile.RestApi/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using Microsoft.OpenApi.Models;
-using Microsoft.Extensions.Options;
 
 namespace Fakebook.Profile.RestApi
 {
