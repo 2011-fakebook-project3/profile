c3a01a9944ead97bea7b96b3b92cdc970baf1f75
dppet <dppet@DANIELOMEN>
2020-12-31 07:24:39 -0800

    placeholder cors for okta

--- Fakebook.Profile/Fakebook.Profile.RestApi/Startup.cs
+++ Fakebook.Profile/Fakebook.Profile.RestApi/Startup.cs
@@ -1,11 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Options;
+using Fakebook.Profile.DataAccess.Services;
 
 namespace Fakebook.Profile.RestApi
 {
@@ -19,6 +27,28 @@ namespace Fakebook.Profile.RestApi
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+
+            // https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mongo-app?view=aspnetcore-5.0&tabs=visual-studio#add-a-configuration-model
+            services.Configure<ProfileDbSettings>(
+                Configuration.GetSection(nameof(ProfileDbSettings)));
+
+            services.AddSingleton<IProfileDbSettings>(sp =>
+                sp.GetRequiredService<IOptions<ProfileDbSettings>>().Value);
+
+
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:4200", "https://frontend.site", "http://other.services", "okta.site")
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                    });
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fakebook.ProfileRestApi", Version = "v1" });
