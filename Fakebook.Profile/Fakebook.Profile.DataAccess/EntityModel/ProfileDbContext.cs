b8f5a7d879ffe77a1d1081eacf13aef46cdb6c2c
dppet <dppet@DANIELOMEN>
2021-01-05 08:31:40 -0800

    copied over skeleton for db context

---
+++ Fakebook.Profile/Fakebook.Profile.DataAccess/EntityModel/ProfileDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fakebook.Profile.DataAccess.EntityModel
+{
+    class ProfileDbContext : DbContext
+    {
+
+        public ProfileDbContext([NotNull] DbContextOptions options) :
+            base(options)
+        { }
+
+        public DbSet<EntityProfile> StorageProfiles { get; set; }
+
+        public DbSet<EntityFollow> Follows { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            throw new NotImplementedException();
+        }
+    }
+}
