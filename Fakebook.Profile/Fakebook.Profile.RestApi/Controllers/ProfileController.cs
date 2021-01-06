3b7faba436a4288da5deab7b8d84ce7326256b30
Damien Bevins <damien.bevins@outlook.com>
2021-01-06 11:50:52 -0600

    Add endpoints in the ProfileController without any implementation.

--- Fakebook.Profile/Fakebook.Profile.RestApi/Controllers/ProfileController.cs
+++ Fakebook.Profile/Fakebook.Profile.RestApi/Controllers/ProfileController.cs
@@ -2,14 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using Fakebook.Profile.RestApi.ApiModel;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fakebook.Profile.RestApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/profile")]
     [ApiController]
     public class ProfileController : ControllerBase
     {
+        // PUT: /api/profile/selection/{emails}
+        [HttpGet("selection/{emails}")]
+        public ActionResult<IEnumerable<ProfileApiModel>> SelectProfiles([FromBody] IEnumerable<string> emails)
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET: /api/profile/{userEmail}
+        [HttpGet("{userEmail}")]
+        public ActionResult<ProfileApiModel> Get(string userEmail = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST: /api/profile/
+        [HttpPost]
+        public ActionResult Create([FromBody] ProfileApiModel apiModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT: /api/profile/
+        [HttpPut]
+        public ActionResult Update([FromBody] ProfileApiModel apiModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
