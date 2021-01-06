158223ba02f5964016918d10ef31e0a6f6a36788
Damien Bevins <damien.bevins@outlook.com>
2021-01-06 11:41:24 -0600

    Moved regular expressions to domain project

---
+++ Fakebook.Profile/Fakebook.Profile.Domain/Utility/RegularExpressions.cs
@@ -0,0 +1,10 @@
+﻿namespace Fakebook.Profile.Domain.Utility
+{
+    public static class RegularExpressions
+    {
+        public const string NoSpecialCharacters = @"^[A-Za-z0-9 '\-_%,.#?:!/]*$";
+        public const string NameCharacters = @"^[A-Za-z0-9 -]*$";
+        public const string EmailCharacters = @"^[a-zA-Z]+[a-zA-Z0-9\.]*[a-zA-Z0-9]*\@[a-zA-Z]+[a-zA-Z0-9]*\.([a-zA-Z]+){0,253}$";
+        public const string PhoneNumberCharacters = @"^(\([0-9]{3}\)|[0-9]{3})?[ -]?[0-9]{3}[ -]?[0-9]{4}$";
+    }
+}
