c0f0d82f372700de31fa28a1946a679d541a3304
Damien Bevins <damien.bevins@outlook.com>
2021-01-07 11:28:22 -0600

    Changed 'StorageModelProfileTests' to match the entity class, 'EntityProfile' (with the new file being 'EntityProfileTests').

--- Fakebook.Profile/Fakebook.Profile.UnitTests/DataAccessTests/StorageModelTests/EntityProfileTests.cs
+++ Fakebook.Profile/Fakebook.Profile.UnitTests/DataAccessTests/StorageModelTests/EntityProfileTests.cs
@@ -9,7 +9,7 @@ using Xunit;
 
 namespace Fakebook.Profile.DataAccess.StorageModel
 {
-    public class StorageModelProfileTests
+    public class EntityProfileTests
     {
         //Email Tests
         /// <summary>
