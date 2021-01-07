a75c01ae9c1a34ddc9e9e250ce6ee3ab4a59c10d
Damien Bevins <damien.bevins@outlook.com>
2021-01-07 15:30:25 -0600

    Changed connection string name to fit the solution name

--- Fakebook.Profile/Fakebook.Profile.RestApi/Startup.cs
+++ Fakebook.Profile/Fakebook.Profile.RestApi/Startup.cs
@@ -28,7 +28,7 @@ namespace Fakebook.Profile.RestApi
             */
 
             services.AddDbContext<ProfileDbContext>(options
-                => options.UseNpgsql(Configuration["Fakebook:ConnectionString"]));
+                => options.UseNpgsql(Configuration["FakebookProfile:ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
