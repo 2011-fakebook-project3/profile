1f34cf35568ffe2c6e6396af0a2c40e1ad5d1244
Damien Bevins <damien.bevins@outlook.com>
2021-01-07 13:49:07 -0600

    Added missing 'ToDomainProfile' from ProfileApiModel

--- Fakebook.Profile/Fakebook.Profile.RestApi/ApiModel/ProfileApiModel.cs
+++ Fakebook.Profile/Fakebook.Profile.RestApi/ApiModel/ProfileApiModel.cs
@@ -11,17 +11,6 @@ namespace Fakebook.Profile.RestApi.ApiModel
     /// </summary>
     public class ProfileApiModel
     {
-        public ProfileApiModel(DomainProfile p)
-        {
-            Email = p.Email;
-            ProfilePictureUrl = p.ProfilePictureUrl;
-            FirstName = p.FirstName;
-            LastName = p.LastName;
-            PhoneNumber = p.PhoneNumber;
-            BirthDate = p.BirthDate;
-            Status = p.Status;
-        }
-
         //[anything]@[anything].[anything]
         [Required, RegularExpression(RegularExpressions.EmailCharacters)]
         public string Email { get; set; }
@@ -45,5 +34,30 @@ namespace Fakebook.Profile.RestApi.ApiModel
 
         //can be null, or reasonable text (sanitized so they don't get funky)
         public string Status { get; set; }
+
+        public ProfileApiModel(DomainProfile p)
+        {
+            Email = p.Email;
+            ProfilePictureUrl = p.ProfilePictureUrl;
+            FirstName = p.FirstName;
+            LastName = p.LastName;
+            PhoneNumber = p.PhoneNumber;
+            BirthDate = p.BirthDate;
+            Status = p.Status;
+        }
+
+        public DomainProfile ToDomainProfile()
+        {
+            return new DomainProfile
+            {
+                Email = Email,
+                FirstName = FirstName,
+                LastName = LastName,
+                PhoneNumber = PhoneNumber,
+                BirthDate = BirthDate,
+                ProfilePictureUrl = ProfilePictureUrl,
+                Status = Status
+            };
+        }
     }
 }
