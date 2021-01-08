5749c2581d4aff14fee1e610c5e24852f83f310f
Hao <haoyang439@gmail.com>
2021-01-08 09:00:33 -0700

    Added migration, updated and seeded the database

--- Fakebook.Profile/Fakebook.Profile.RestApi/Startup.cs
+++ Fakebook.Profile/Fakebook.Profile.RestApi/Startup.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Fakebook.Profile.DataAccess.EntityModel;
+using Fakebook.Profile.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,16 @@ namespace Fakebook.Profile.RestApi
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+
+            var connectionString = Configuration.GetConnectionString("default");
+            if (connectionString is null)
+            {
+                throw new InvalidOperationException("no connection string 'default' found");
+            }
+            services.AddDbContext<ProfileDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddScoped<IProfileRepository, ProfileRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c => {
