4315af7da00e3107757cb9633bffb31caf491e3e
Damien Bevins <damien.bevins@outlook.com>
2021-01-08 15:39:25 -0500

    Add StorageService as a transient throughout the application in the Startup.cs file

--- Fakebook.Profile/Fakebook.Profile.RestApi/Startup.cs
+++ Fakebook.Profile/Fakebook.Profile.RestApi/Startup.cs
@@ -72,6 +72,8 @@ namespace Fakebook.Profile.RestApi
 
             services.AddDbContext<ProfileDbContext>(options
                 => options.UseNpgsql(Configuration["FakebookProfile:ConnectionString"]));
+
+            services.AddTransient<IStorageService, StorageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
