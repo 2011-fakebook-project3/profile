e269e3af092391793bb5f2c9f34f9da8efc7acf3
Damien Bevins <damien.bevins@outlook.com>
2021-01-08 15:56:44 -0500

    Add test files that were missing from the us-account-login

---
+++ Fakebook.Profile/Fakebook.Profile.UnitTests/DomainTests/ProfileRandomizedTests.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Fakebook.Profile.Domain;
+using Fakebook.Profile.Domain.Utility;
+using Fakebook.Profile.UnitTests.TestData;
+using Fakebook.Profile.UnitTests.TestData.ProfileTestData;
+
+using Xunit;
+
+namespace Fakebook.Profile.UnitTests.DomainTests
+{
+    // unit testing domain profile model
+    public class ProfileRandomizedTests
+    {
+        /*
+        * User:
+        * - Email: string
+        * - ProfilePictureUrl: Uri
+        * - Name : string
+        * - FirstName: string
+        * - Lastname: string              
+        * - PhoneNumber: string
+        * - BirthDate: DateTime
+        * - Status: string
+        */
+
+        [Theory]
+        [ClassData(typeof(Create.Valid))]
+        public void ValidUserInfoShouldReturnWithoutErrors(DomainProfile MockedProfile)
+        {
+            // arrange
+            DomainProfile profile = new DomainProfile();
+
+            // act
+            profile.Email = MockedProfile.Email;
+            profile.ProfilePictureUrl = MockedProfile.ProfilePictureUrl;
+            profile.FirstName = MockedProfile.FirstName;
+            profile.LastName = MockedProfile.LastName;
+            profile.PhoneNumber = MockedProfile.PhoneNumber;
+            profile.BirthDate = MockedProfile.BirthDate;
+            profile.Status = MockedProfile.Status;
+
+            // assert
+            Assert.NotNull(profile.Name);
+            Assert.NotNull(profile.FirstName);
+            Assert.NotNull(profile.LastName);
+            Assert.Matches(RegularExpressions.EmailCharacters, profile.Email);
+            Assert.Matches(RegularExpressions.PhoneNumberCharacters, profile.PhoneNumber);
+        }
+
+        [Theory]
+        [ClassData(typeof(Create.InvalidName))]
+        public void InvalidUserNameReturnsErrors(DomainProfile MockedProfile)
+        {
+            // arrange
+            DomainProfile profile = new DomainProfile();
+
+            // act
+            profile.Email = MockedProfile.Email;
+            profile.ProfilePictureUrl = MockedProfile.ProfilePictureUrl;
+            // profile.FirstName = MockedProfile.FirstName;
+            // profile.LastName = MockedProfile.LastName;
+            profile.PhoneNumber = MockedProfile.PhoneNumber;
+            profile.BirthDate = MockedProfile.BirthDate;
+            profile.Status = MockedProfile.Status;
+            
+            // assert
+            //Assert.Null(profile.Name);
+            //Assert.Null(profile.FirstName);
+            //Assert.Null(profile.LastName);
+            Assert.Matches(RegularExpressions.EmailCharacters, profile.Email);
+            Assert.Matches(RegularExpressions.PhoneNumberCharacters, profile.PhoneNumber);
+            Assert.ThrowsAny<ArgumentNullException>(() => profile.FirstName = null);
+            Assert.ThrowsAny<ArgumentNullException>(() => profile.LastName = null);
+        }
+
+        [Theory]
+        [ClassData(typeof(Create.InvalidPhoneNumber))]
+        public void InvalidUserPhoneNumberReturnsErrors(DomainProfile MockedProfile)
+        {
+            // arrange
+            DomainProfile profile = new DomainProfile();
+
+            // act
+            profile.Email = MockedProfile.Email;
+            profile.ProfilePictureUrl = MockedProfile.ProfilePictureUrl;
+            profile.FirstName = MockedProfile.FirstName;
+            profile.LastName = MockedProfile.LastName;
+            // profile.PhoneNumber = MockedProfile.PhoneNumber;
+            profile.BirthDate = MockedProfile.BirthDate;
+            profile.Status = MockedProfile.Status;
+
+            // assert
+            Assert.NotNull(profile.Name);
+            Assert.NotNull(profile.FirstName);
+            Assert.NotNull(profile.LastName);
+            Assert.Matches(RegularExpressions.EmailCharacters, profile.Email);
+            // Assert.DoesNotMatch(RegularExpressions.PhoneNumberCharacters, profile.PhoneNumber);
+            Assert.ThrowsAny<ArgumentException>(() => profile.PhoneNumber = GenerateRandom.String());
+        }
+
+        [Theory]
+        [ClassData(typeof(Create.InvalidEmail))]
+        public void InvalidUserEmailReturnsErrors(DomainProfile MockedProfile)
+        {
+            // arrange
+            DomainProfile profile = new DomainProfile();
+
+            // act
+            // profile.Email = MockedProfile.Email;
+            profile.ProfilePictureUrl = MockedProfile.ProfilePictureUrl;
+            profile.FirstName = MockedProfile.FirstName;
+            profile.LastName = MockedProfile.LastName;
+            profile.PhoneNumber = MockedProfile.PhoneNumber;
+            profile.BirthDate = MockedProfile.BirthDate;
+            profile.Status = MockedProfile.Status;
+
+            // assert
+            Assert.NotNull(profile.Name);
+            Assert.NotNull(profile.FirstName);
+            Assert.NotNull(profile.LastName);
+            Assert.Matches(RegularExpressions.PhoneNumberCharacters, profile.PhoneNumber);
+            // Assert.DoesNotMatch(RegularExpressions.EmailCharacters, profile.Email);
+            Assert.ThrowsAny<ArgumentException>(() => profile.Email = GenerateRandom.String());
+        }
+
+        [Theory]
+        [InlineData("test.com", "test")]
+        [InlineData("i.imgur.com", "BCeyxdR.jpg")]
+        [InlineData("i.imgur.com", "Lf5S5Sa.jpg")]
+        [InlineData("i.imgur.com", "DX5KAnQ.jpg")]
+        public void SetUri(string domain, string path)
+        {
+            // arrange
+            DomainProfile profile = new DomainProfile();
+
+            //act
+            var builder = new UriBuilder();
+            builder.Host = domain;
+            builder.Path = path;
+            profile.ProfilePictureUrl = builder.Uri;
+
+            //assert
+            Assert.NotNull(profile.ProfilePictureUrl);
+            Assert.Equal(domain, profile.ProfilePictureUrl.Host);
+            Assert.EndsWith(path, profile.ProfilePictureUrl.AbsoluteUri);
+        }
+    }
+}
---
+++ Fakebook.Profile/Fakebook.Profile.UnitTests/DomainTests/RepositoryTests/CreateTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Fakebook.Profile.DataAccess.EntityModel;
+using Fakebook.Profile.Domain;
+
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+using Xunit;
+
+namespace Fakebook.Profile.UnitTests.DataAccessTests.RepositoryTests
+{
+    public class CreateTest
+    {
+        //create tests
+        #region CreationTests
+        [Fact]
+        public async void CreateValidRepoWorks()
+        {
+            //arrange
+            using var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<ProfileDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            //TODO: either make this an arg, and theory function, or fill in data
+            DomainProfile newProfile = new DomainProfile();
+
+            //act
+            using (var testContext = new ProfileDbContext(options))
+            {
+                testContext.Database.EnsureCreated();
+
+                var repo = new ProfileRepository(testContext);
+
+                // Create the user data
+                await repo.CreateProfileAsync(newProfile);
+            }
+
+            //assert
+            using (var assertcontext = new ProfileDbContext(options))
+            {
+                var repo = new ProfileRepository(assertcontext);
+
+                DomainProfile profile = await repo.GetProfileAsync(newProfile.Email);
+
+                Assert.NotNull(profile);
+
+                //assert fields are the same
+                Assert.Equal(newProfile.BirthDate, profile.BirthDate);
+                Assert.Equal(newProfile.Status, profile.Status);
+                Assert.Equal(newProfile.FirstName, profile.FirstName);
+                Assert.Equal(newProfile.LastName, profile.LastName);
+                Assert.Equal(newProfile.PhoneNumber, profile.PhoneNumber);
+                Assert.Equal(newProfile.ProfilePictureUrl, profile.ProfilePictureUrl);
+            }
+
+        }
+
+        [Fact]
+        public void CreateInvalidUserFails()
+        {
+            // Arrange
+            using var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<ProfileDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            //invalid profile
+            DomainProfile invalid = new DomainProfile();
+
+            // Act
+            using (var actingContext = new ProfileDbContext(options))
+            {
+                actingContext.Database.EnsureCreated();
+
+                var repo = new ProfileRepository(actingContext);
+
+                // Create the user data
+                Assert.ThrowsAsync<ArgumentException>(() => repo.CreateProfileAsync(invalid));
+            }
+
+            //assert
+            using (var assertionContext = new ProfileDbContext(options))
+            {
+                var repo = new ProfileRepository(assertionContext);
+
+                var profile = repo.GetProfileAsync(invalid.Email);
+
+                Assert.Null(profile);
+            }
+        }
+
+        #endregion
+    }
+}
