21b481b0d629d05549921afd3dbb57c9f25278f5
dppet <dppet@DANIELOMEN>
2021-01-11 14:13:59 -0800

    doc string on interface function.

--- Fakebook.Profile/Fakebook.Profile.DataAccess/Services/Interfaces/IStorageService.cs
+++ Fakebook.Profile/Fakebook.Profile.DataAccess/Services/Interfaces/IStorageService.cs
@@ -9,6 +9,14 @@ namespace Fakebook.Profile.DataAccess.Services.Interfaces
     /// </summary>
     public interface IStorageService
     {
+        /// <summary>
+        /// Uploads a file to remote storage for the app.
+        /// </summary>
+        /// <param name="content">A file stream of an image file.</param>
+        /// <param name="contentType">The content type. (possibly file format)</param>
+        /// <param name="fileName">The name of the file to save</param>
+        /// <param name="ContainerName">(Optional) The container in which to upload the file to.</param>
+        /// <returns></returns>
         public Task<Uri> UploadFileContentAsync(Stream content, string contentType, string fileName, string ContainerName = null);
     }
 }
