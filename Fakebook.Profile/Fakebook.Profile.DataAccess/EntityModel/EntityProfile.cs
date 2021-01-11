e68c54784f913b887bde569937db9e0a79b65c3f
dppet <dppet@DANIELOMEN>
2021-01-11 14:03:13 -0800

    doc strings for entity profile.

--- Fakebook.Profile/Fakebook.Profile.DataAccess/EntityModel/EntityProfile.cs
+++ Fakebook.Profile/Fakebook.Profile.DataAccess/EntityModel/EntityProfile.cs
@@ -6,28 +6,56 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Fakebook.Profile.DataAccess.EntityModel
 {
+    /// <summary>
+    /// Database model of a profile. 
+    /// </summary>
     public class EntityProfile
     {
+        /// <summary>
+        /// Copy the profile entity.
+        /// </summary>
+        /// <returns>A copy with the same values for the fields.</returns>
         public EntityProfile ShallowCopy()
         {
             return (EntityProfile)this.MemberwiseClone();
         }
 
+        /// <summary>
+        /// The primary key, the user's email.
+        /// </summary>
         [Key]
         [Column(name: nameof(Email))]
         public string Email { get; set; }
 
+        /// <summary>
+        /// The Uri of the user's profile picture. Can be null.
+        /// </summary>
         public Uri ProfilePictureUrl { get; set;}
 
+        /// <summary>
+        /// The user's first name. Cannot be null.
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// The user's last name, cannot be null.
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// The user's phone number. can be upto 15 characters. (?) Can be null. 
+        /// </summary>
         [MaxLength(15)]
         public string PhoneNumber { get; set; }
 
+        /// <summary>
+        /// The user's birthdate. Can be null.
+        /// </summary>
         public DateTime BirthDate { get; set; }
 
+        /// <summary>
+        /// The user's current status message, can be null.
+        /// </summary>
         public string Status { get; set; }
     }
 }
