fb44cfe5f0dd474151bf560a9eb174c85fc5f55a
dppet <dppet@DANIELOMEN>
2021-01-12 08:41:45 -0800

    fully cleaned up

--- Fakebook.Profile/Fakebook.Profile.Domain/IProfileRepository.cs
+++ Fakebook.Profile/Fakebook.Profile.Domain/IProfileRepository.cs
@@ -13,23 +13,27 @@ namespace Fakebook.Profile.Domain
         /// </summary>
         /// <param name="profileData">Domain profile used.</param>
         Task CreateProfileAsync(DomainProfile profileData);
+
         /// <summary>
         /// Get one specific user profile using their email.
         /// </summary>
         /// <param name="email">Email used to find the specific user.</param>
-
+        /// <returns>A specific profile with the matching email.</returns>
         Task<DomainProfile> GetProfileAsync(string email);
+
         /// <summary>
         /// Get a group of user profiles using their emails.
         /// </summary>
         /// <param name="emails">A collection of emails used to find users.</param>
         /// <returns>A collection of domain profiles matching the emails provided.</returns>
         Task<IEnumerable<DomainProfile>> GetProfilesByEmailAsync(IEnumerable<string> emails);
+
         /// <summary>
         /// Get all users' profiles at once.
         /// </summary>
         /// <returns>A list of all matching profiles from the database.</returns>
         Task<IEnumerable<DomainProfile>> GetAllProfilesAsync();
+
         /// <summary>
         /// Updates a user's profile with the information provided.
         /// </summary>
