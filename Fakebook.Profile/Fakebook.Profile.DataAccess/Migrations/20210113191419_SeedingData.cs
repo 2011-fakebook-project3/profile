387c176f8112c4b51f5cf851f646390552dc426f
Damien Bevins <damien.bevins@outlook.com>
2021-01-13 14:21:41 -0500

    update the migration to use the more recent version, and it uses the seed data

---
+++ Fakebook.Profile/Fakebook.Profile.DataAccess/Migrations/20210113191419_SeedingData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fakebook.Profile.DataAccess.Migrations
+{
+    public partial class SeedingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Fakebook");
+
+            migrationBuilder.CreateTable(
+                name: "Profile",
+                schema: "Fakebook",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    ProfilePictureUrl = table.Column<string>(type: "text", nullable: true),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profile", x => x.Email);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "Fakebook",
+                table: "Profile",
+                columns: new[] { "Email", "BirthDate", "FirstName", "LastName", "PhoneNumber", "ProfilePictureUrl", "Status" },
+                values: new object[,]
+                {
+                    { "david.barnes@revature.net", new DateTime(1994, 6, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), "David", "Barnes", null, "https://images.unsplash.com/photo-1489533119213-66a5cd877091", "deployed my app feeling good about today's presentation" },
+                    { "testaccount@gmail.com", new DateTime(1994, 9, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), "Jay", "Shin", null, "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/252252921/1800", null }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profile",
+                schema: "Fakebook");
+        }
+    }
+}
