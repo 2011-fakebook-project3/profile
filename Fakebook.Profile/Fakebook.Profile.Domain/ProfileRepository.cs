1a089d1cfbb37fcc7f5e6899d2eaf1eb0552dc17
Damien Bevins <damien.bevins@outlook.com>
2021-01-14 01:37:03 -0500

    Fix to the different casings used between the db and asp.net core's authentication

--- Fakebook.Profile/Fakebook.Profile.Domain/ProfileRepository.cs
+++ Fakebook.Profile/Fakebook.Profile.Domain/ProfileRepository.cs
@@ -107,7 +107,7 @@ namespace Fakebook.Profile.Domain
             }
 
             var profile = await _context.EntityProfiles
-                .FirstOrDefaultAsync(x => x.Email == email);
+                .FirstOrDefaultAsync(x => x.Email.ToLower() == email.ToLower());
 
             if (profile == null)
             {
@@ -128,10 +128,10 @@ namespace Fakebook.Profile.Domain
         {
             var userEntities = _context.EntityProfiles;
             var userEmails = userEntities
-                .Select(u => u.Email)
+                .Select(u => u.Email.ToLower())
                 .ToList();
 
-            if (!emails.All(e => userEmails.Contains(e)))
+            if (!emails.All(e => userEmails.Contains(e.ToLower())))
             {
                 throw new ArgumentException("Not all emails requested are present.", nameof(emails));
             }
@@ -188,7 +188,9 @@ namespace Fakebook.Profile.Domain
                     throw new ArgumentException("Source is empty", nameof(entities));
                 }
 
-                var profile = await _context.EntityProfiles.FirstOrDefaultAsync(x => x.Email == email);
+                var profile = await _context.EntityProfiles
+                    .FirstOrDefaultAsync(x => x.Email.ToLower() == email.ToLower());
+
                 if (profile == null)
                 {
                     throw new ArgumentNullException("Email not found", nameof(email));
