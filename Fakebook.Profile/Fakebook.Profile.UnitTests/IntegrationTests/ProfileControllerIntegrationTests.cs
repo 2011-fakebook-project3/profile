054510bfed80b020dede108ddcd87e9a2b4a3b6d
Trevor Dunbar <tdunbar123@yahoo.com>
2021-04-15 13:29:43 -0400

    Added method for unfollowing a user in profile controller along with integration test.

--- Fakebook.Profile/Fakebook.Profile.UnitTests/IntegrationTests/ProfileControllerIntegrationTests.cs
+++ Fakebook.Profile/Fakebook.Profile.UnitTests/IntegrationTests/ProfileControllerIntegrationTests.cs
@@ -51,5 +51,56 @@ namespace Fakebook.Profile.UnitTests.IntegrationTests
             var result = await controller.Follow(test.Email);
             Assert.IsAssignableFrom<OkResult>(result);
         }
+
+        [Fact]
+        public async Task UnfollowAsync()
+        {
+            using var contextFactory = new ContextFactory();
+            using var context = contextFactory.CreateContext();
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, "john werner"),
+                new Claim(ClaimTypes.NameIdentifier, "john.werner@revature.net"),
+                new Claim("custom-claim", "example claim value"),
+            }, "mock"));
+
+            var repo = new ProfileRepository(context);
+            Mock<IStorageService> mockedStorageService = new();
+            ProfileController controller = new(
+                repo,
+                mockedStorageService.Object,
+                new NullLogger<ProfileController>()
+            );
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+
+            DomainProfile test = new DomainProfile("tdunbar@google.com", "Trevor", "Dunbar");
+            test.BirthDate = new DateTime(1994, 6, 30, 0, 0, 0, 0, DateTimeKind.Unspecified);
+
+            await repo.CreateProfileAsync(test);
+
+            DomainProfile userDomain = await repo.GetProfileAsync("john.werner@revature.net");
+
+            // Make userDomain follow test user
+
+            userDomain.AddFollow(test.Email);
+            test.AddFollower(userDomain.Email);
+
+            // Update database
+            await repo.UpdateProfileAsync(userDomain.Email, userDomain);
+            await repo.UpdateProfileAsync(test.Email, test);
+
+            // Unfollow test user and assert
+            var result = await controller.Unfollow(test.Email);
+            userDomain = await repo.GetProfileAsync(userDomain.Email);
+            test = await repo.GetProfileAsync(test.Email);
+
+            Assert.IsAssignableFrom<OkResult>(result);
+            Assert.DoesNotContain(test.Email, userDomain.FollowingEmails);
+            Assert.DoesNotContain(userDomain.Email, test.FollowerEmails);
+        }
     }
 }
